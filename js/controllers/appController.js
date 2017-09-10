a93bb9dbec86a82a3a8dfea6cd875af9bedfbfb6
John Zhao <johnnyzhao29@hotmail.com>
2017-09-10 16:52:45 -0400

    Standard first name.

--- js/controllers/appController.js
+++ js/controllers/appController.js
@@ -1,7 +1,6 @@
 app.controller('myCtrl', function ($scope) {
-  $scope.firstName = 'XINUO';
-  $scope.nickName =  'JOHN';
-  $scope.lastName = 'ZHAO';
-  $scope.fullName = $scope.nickName + ' (' + $scope.firstName + ') ' + $scope.lastName;
+  $scope.firstName = 'John';
+  $scope.lastName = 'Zhao';
+  $scope.fullName = $scope.firstName + ' ' + $scope.lastName;
   $scope.jobName = 'Electrical Engineer and Web Developer';
 });
