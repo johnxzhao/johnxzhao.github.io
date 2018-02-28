862a9f2e1672837b5209611e212b999dc224f586
John Zhao <john.zhao@provisionusa.com>
2018-02-28 12:29:01 -0500

    Add chinese content. Organize folder structure.

--- scripts/app/controllers/appController.js
+++ scripts/app/controllers/appController.js
@@ -1,6 +1,8 @@
 app.controller('myCtrl', function ($scope) {
   $scope.firstName = 'John';
   $scope.lastName = 'Zhao';
+  $scope.chineseName = '赵夕诺';
   $scope.fullName = $scope.firstName + ' ' + $scope.lastName;
-  $scope.jobName = 'Electrical Engineer and Web Developer';
+  $scope.jobName = 'Electronic Engineer and Web Developer';
+  $scope.chineseJobName = '电子工程师 / 网页开发者';
 });
