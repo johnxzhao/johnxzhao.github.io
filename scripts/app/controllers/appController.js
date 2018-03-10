f2ccd835f33b75b1dbff973bf76a65d5af6bb25b
John Zhao <johnxzhao@johns-mbp.lan>
2018-03-10 16:17:56 -0500

    Add responsive communication icons.

--- scripts/app/controllers/appController.js
+++ scripts/app/controllers/appController.js
@@ -1,3 +1,5 @@
+var app = angular.module('myApp', ['ngAnimate']);
+
 app.controller('myCtrl', function ($scope) {
   $scope.firstName = 'John';
   $scope.lastName = 'Zhao';
@@ -5,4 +7,5 @@ app.controller('myCtrl', function ($scope) {
   $scope.fullName = $scope.firstName + ' ' + $scope.lastName;
   $scope.jobName = 'Electronic Engineer and Web Developer';
   $scope.chineseJobName = '电子工程师 / 网页开发者';
+  $scope.showWechatQR = true;
 });
